4b3d1365386b08eb38ce67b31534c4aca65ff1e8
AzureHex <183492338+AzureHex@users.noreply.github.com>
2024-11-24 12:36:35 +0530

    Added plugins

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,12 @@ return {
     "stevearc/oil.nvim",
     config = function()
         local oil = require("oil")
-        oil.setup()
+        oil.setup({
+            float = {
+                max_width = 100,
+                max_height = 40,
+            },
+        })
         vim.keymap.set("n", "-", oil.toggle_float, {})
     end,
 }
