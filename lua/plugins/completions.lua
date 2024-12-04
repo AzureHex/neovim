2a7609007c4d965a44b83ac7a901f1f93af4df9b
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-12-05 04:20:29 +0530

    Added cmdline completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -41,4 +41,31 @@ return {
             })
         end,
     },
+    {
+        "hrsh7th/cmp-cmdline",
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
+            })
+        end,
+    },
 }
