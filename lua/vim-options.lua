ae58f57871bd8c8262c159c55e5b850b06419c32
Wolf <183492338+AzureHex@users.noreply.github.com>
2024-12-16 05:00:16 +0530

    Changed keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,13 +6,26 @@ vim.g.mapleader = " "
 
 vim.opt.swapfile = false
 
-vim.api.nvim_set_keymap('n', 'j', 'k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true, silent = true })
-
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-k>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- Normal mode
+vim.api.nvim_set_keymap('n', 'j', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'j', { noremap = true, silent = true })
+
+-- Visual mode
+vim.api.nvim_set_keymap('v', 'j', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'k', 'j', { noremap = true, silent = true })
+
+-- Insert mode
+vim.api.nvim_set_keymap('i', 'j', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'k', 'j', { noremap = true, silent = true })
+
+-- Command-line mode
+vim.api.nvim_set_keymap('c', 'j', 'k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('c', 'k', 'j', { noremap = true, silent = true })
 
 vim.wo.number = true
