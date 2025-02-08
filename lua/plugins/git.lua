1a8c7b89e9a913f774bdf370c42fa8b365067090
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-02-09 05:04:13 +0530

    ✨ feat(neovim): added diffview plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,5 +10,18 @@ return {
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
         end
+    },
+    {
+        "sindrets/diffview.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("diffview").setup({
+                -- You can customize options here if needed
+            })
+
+            -- Example keybindings for diffview
+            vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {})
+            vim.keymap.set("n", "<leader>gq", ":DiffviewClose<CR>", {})
+        end
     }
 }
